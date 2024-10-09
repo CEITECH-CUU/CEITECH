070bd62ce9a002226c2e593a366283e2be3436b3
Jose Fabio <J053Fabi0@users.noreply.github.com>
2024-10-09 14:41:01 -0600

    Title with background and curso page

--- scripts/main.js
+++ scripts/main.js
@@ -70,3 +70,16 @@ function restore(id) {
     history.replaceState(null, null, document.location.pathname);
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let mouseX, mouseY;
+  let traX, traY;
+
+  document.addEventListener("mousemove", function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = (4 * mouseX) / 570 + 40;
+    traY = (4 * mouseY) / 570 + 50;
+    document.querySelector(".title").style.backgroundPosition = traX + "%" + traY + "%";
+  });
+});
